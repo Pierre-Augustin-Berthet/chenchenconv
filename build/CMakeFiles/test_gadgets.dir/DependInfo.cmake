1c04c1fbff27d54fb2d8f8bb1cbaac84bda66ac6
Pierre-Augustin-Berthet <pierre.augustin.berthet@gmail.com>
2024-04-16 10:23:07 +0200

    Revert "Delete build directory"
    This reverts commit 0d085f930ffce67cbf50821169c7145a258daa16.

---
+++ build/CMakeFiles/test_gadgets.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dr4/Documents/chenchenconv/apps/test_gadgets.c" "/home/dr4/Documents/chenchenconv/build/CMakeFiles/test_gadgets.dir/apps/test_gadgets.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dr4/Documents/chenchenconv/build/CMakeFiles/toto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
